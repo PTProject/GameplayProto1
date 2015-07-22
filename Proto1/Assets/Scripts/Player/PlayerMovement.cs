f49366124d5a0bc572cd5ea3d594330ed7a97920
SunnyLannie <kiqnguye@ucsc.edu>
2015-07-21 23:23:43 -0700

    working on basic not-finalized player movement

---
+++ Proto1/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float speed;
+	Vector3 direction;
+	Rigidbody rigidbody;
+	int floorMask;
+
+	void Start () {
+		rigidbody = GetComponent <Rigidbody>();
+		float speed = 5f;
+	}
+
+	void Update () {
+	
+	}
+}
