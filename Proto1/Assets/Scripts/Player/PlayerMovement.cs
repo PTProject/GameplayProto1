9164bf4063aeabec0eddaf157cc87e842c9944dd
SunnyLannie <kiqnguye@ucsc.edu>
2015-07-24 10:28:54 -0700

    rts mouse control ok!

--- Proto1/Assets/Scripts/Player/PlayerMovement.cs
+++ Proto1/Assets/Scripts/Player/PlayerMovement.cs
@@ -20,7 +20,6 @@ public class PlayerMovement : MonoBehaviour {
 
 	void FixedUpdate () {
 		getInput("RTSmouse");
-		Debug.Log (rb.velocity.sqrMagnitude);
 
 	}
 
@@ -63,15 +62,21 @@ public class PlayerMovement : MonoBehaviour {
 
 public static class rbHelper {
 	
+	static float lastSqrMag;
 
 	public static bool moveTo(this Rigidbody rb, Vector3 to, Vector3 velocity,
 	                          float speed){
 		Vector3 distance = to - rb.position;
-		if (distance.sqrMagnitude < velocity.magnitude) {
+		float sqrMag = distance.sqrMagnitude;
+		Debug.Log (distance.sqrMagnitude + " " + velocity.sqrMagnitude + " " + lastSqrMag);
+		if (sqrMag < lastSqrMag || lastSqrMag == 0) {
 			rb.AddForce(velocity, ForceMode.VelocityChange);
+			lastSqrMag = sqrMag;
+			return false;
 		} else {
-			rb.AddForce (velocity, ForceMode.VelocityChange);
+			rb.velocity = Vector3.zero;
+			lastSqrMag = 0;
 		}
-		return false;
+		return true;
 	}
 }
